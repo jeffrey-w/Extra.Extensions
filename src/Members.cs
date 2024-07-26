ebc58640f24b73130df3db4bd61054cd3df5b0c1
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2024-07-26 19:55:07 -0400

    rename class

---
+++ src/Members.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+
+namespace Extensions;
+
+/// <summary>
+/// The <c>MemberInfo</c> class provides additional operations on the <see cref="MemberInfo"/> type.
+/// </summary>
+public static class Members
+{
+    /// <summary>
+    /// Determines whether this <see cref="MemberInfo"/> has the specified <see cref="Attribute"/>,
+    /// <typeparamref name="TAttribute"/>.
+    /// </summary>
+    /// <typeparam name="TAttribute">The <see cref="Type"/> of attribute that this <see cref="MemberInfo"/> is being
+    /// queried for.</typeparam>
+    /// <param name="member">This <see cref="MemberInfo"/>.</param>
+    /// <returns><c>true</c> if this <see cref="MemberInfo"/> has the specified <see cref="Attribute"/>.</returns>
+    public static bool HasCustomAttribute<TAttribute>(this MemberInfo member) where TAttribute : Attribute
+    {
+        return member.GetCustomAttribute<TAttribute>() is not null;
+    }
+
+    /// <summary>
+    /// Determines whether this <see cref="Type"/> is static.
+    /// </summary>
+    /// <param name="type">This <see cref="Type"/>.</param>
+    /// <returns><c>true</c> if this <see cref="Type"/> is static.</returns>
+    public static bool IsStatic(this Type type)
+    {
+        return type is { IsAbstract: true, IsSealed: true };
+    }
+}
