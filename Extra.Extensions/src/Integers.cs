b1ea1563d22dcd9fac904e5fd898e48e5958538c
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2025-08-25 22:02:25 -0400

    add `IsPredecessor`

--- Extra.Extensions/src/Integers.cs
+++ Extra.Extensions/src/Integers.cs
@@ -7,7 +7,22 @@ namespace Extra.Extensions;
 public static class Integers
 {
     /// <summary>
-    /// Determines whether this <see cref="int" /> is one greater the
+    /// Determines whether this <see cref="int" /> is one less than the
+    /// <paramref name="other" /> one.
+    /// </summary>
+    /// <param name="i"> This <see cref="int" />.</param>
+    /// <param name="other">The <see cref="int" /> to compare against this one</param>
+    /// <returns>
+    /// <c>true</c> if this <see cref="int" /> is one less than the
+    /// <paramref name="other" /> one.
+    /// </returns>
+    public static bool IsPredecessor(this int i, int other)
+    {
+        return other - 1 == i;
+    }
+    
+    /// <summary>
+    /// Determines whether this <see cref="int" /> is one greater than the
     /// <paramref name="other" /> one.
     /// </summary>
     /// <param name="i"> This <see cref="int" />.</param>
