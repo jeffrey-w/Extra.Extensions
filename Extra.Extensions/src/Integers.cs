b27bea2a4d514164f9e3fcfb45ee2e0bae0be4b5
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2026-03-21 12:14:56 -0400

    add overload for nullable parameter

--- Extra.Extensions/src/Integers.cs
+++ Extra.Extensions/src/Integers.cs
@@ -20,7 +20,7 @@ public static class Integers
     {
         return other - 1 == i;
     }
-    
+
     /// <summary>
     /// Determines whether this <see cref="int" /> is one greater than the
     /// <paramref name="other" /> one.
@@ -43,8 +43,23 @@ public static class Integers
     /// <param name="i">This <see cref="int"/>.</param>
     /// <param name="other">The <see cref="int"/> to compare against this one</param>
     /// <returns><c>true</c> if this <see cref="int"/> is a multiple of the
-    /// <see cref="other"/> one.</returns>
+    /// <paramref name="other"/> one.</returns>
+    [Obsolete(
+        "This method will be removed in a future release. The overload accepting a nullable integer ought to be preferred.")]
     public static bool IsDividedBy(this int i, int other)
+    {
+        return IsDividedBy(i, (int?)other);
+    }
+
+    /// <summary>
+    /// Determines whether this <see cref="int"/> is a multiple of the
+    /// <paramref name="other"/> one.
+    /// </summary>
+    /// <param name="i">This <see cref="int"/>.</param>
+    /// <param name="other">The <see cref="int"/> to compare against this one</param>
+    /// <returns><c>true</c> if this <see cref="int"/> is a multiple of the
+    /// <paramref name="other"/> one.</returns>
+    public static bool IsDividedBy(this int i, int? other)
     {
         return i % other == 0;
     }
