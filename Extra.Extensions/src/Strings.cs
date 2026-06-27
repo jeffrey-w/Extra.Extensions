ddda424aebaae6e4b27df5591bc935a472a93d71
jeffrey-w <39098167+jeffrey-w@users.noreply.github.com>
2026-06-26 21:59:35 -0400

    edit comments

--- Extra.Extensions/src/Strings.cs
+++ Extra.Extensions/src/Strings.cs
@@ -7,8 +7,8 @@ namespace Extra.Extensions;
 public static class Strings
 {
     /// <summary>
-    /// Provides the string produced by surrounding this one with quotation
-    /// marks.
+    /// Provides the <see cref="string"/> produced by surrounding this one with
+    /// quotation marks.
     /// </summary>
     /// <param name="s">This <see cref="string"/>.</param>
     /// <returns>A new <see cref="string"/>.</returns>
